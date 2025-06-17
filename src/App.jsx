7316a217603a6200849a9d9178eab39749cee2d8
= <your_email@example.com>
2025-06-17 23:31:13 +0530

    'fix'

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/Portfolio" element={<Home />} />
         </Routes>
         <Footer />
       </BrowserRouter>
